baf4b24bfe60c9ef49ffb9338d7124b7db515453
Luiz Augusto Marques <luizaugustom030502@gmail.com>
2025-07-24 23:28:05 -0300

    Adicionado createdAt na criacao da capsula

--- src/main/java/time/capsule/api/core/usecase/TimeCapsuleSendUseCase.java
+++ src/main/java/time/capsule/api/core/usecase/TimeCapsuleSendUseCase.java
@@ -3,11 +3,11 @@ package time.capsule.api.core.usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import time.capsule.api.core.domain.TimeCapsuleEntity;
 import time.capsule.api.core.gateway.messaging.TimeCapsuleMessagingGateway;
 import time.capsule.api.core.gateway.repository.TimeCapsuleRepositoryGateway;
 import time.capsule.api.core.usecase.validators.TimeCapsuleValidator;
+import time.capsule.api.core.util.DateUtil;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +20,7 @@ public class TimeCapsuleSendUseCase {
     @Transactional
     public void send(TimeCapsuleEntity timeCapsule) {
         timeCapsuleValidator.validateSend(timeCapsule);
+        timeCapsule.setCreatedAt(DateUtil.getDate());
 
         timeCapsuleRepositoryGateway.save(timeCapsule);
         timeCapsuleMessagingGateway.send(timeCapsule.getId());
